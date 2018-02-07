9883b93e75855a269cd54addcc75cd86a8736c4a
Jordan Zucker <jordan.zucker@gmail.com>
2018-02-07 10:39:48 -0800

    Updated with my build tweaks as well

--- scripts/ci/kubernetes/docker/build.sh
+++ scripts/ci/kubernetes/docker/build.sh
@@ -20,10 +20,7 @@ TAG=${2:-latest}
 DIRNAME=$(cd "$(dirname "$0")"; pwd)
 AIRFLOW_ROOT="$DIRNAME/../../../.."
 
-ENVCONFIG=$(minikube docker-env)
-if [ $? -eq 0 ]; then
-  eval $ENVCONFIG
-fi
+
 
 cd $AIRFLOW_ROOT && python setup.py sdist && cp $AIRFLOW_ROOT/dist/*.tar.gz $DIRNAME/airflow.tar.gz && \
 cd $DIRNAME && \
