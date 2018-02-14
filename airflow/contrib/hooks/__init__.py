d7330f2aaef268063b62cf5f30d30a53e1ba268d
Jordan Zucker <jordan.zucker@gmail.com>
2018-02-14 12:06:31 -0800

    Updated to allow import

--- airflow/contrib/hooks/__init__.py
+++ airflow/contrib/hooks/__init__.py
@@ -50,7 +50,8 @@ _hooks = {
     'fs_hook': ['FSHook'],
     'wasb_hook': ['WasbHook'],
     'gcp_pubsub_hook': ['PubSubHook'],
-    'aws_dynamodb_hook': ['AwsDynamoDBHook']
+    'aws_dynamodb_hook': ['AwsDynamoDBHook'],
+    'segment_hook': ['SegmentHook'],
 }
 
 import os as _os
